598496389784ccf51d305fd558c74c5a23e798bd
Alejandro <agmguindo@gmail.com>
2023-11-12 12:05:30 +0100

    Exercice solved

--- client/index.js
+++ client/index.js
@@ -1,17 +1,39 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
 
-async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
+async function main(name) {
+  if (!name) {
+    console.error("Please provide a name as a command-line argument.");
+    return;
+  }
+  // TODO: how do we prove to the server we're on the nice list?
+  const merkleTree = new MerkleTree(niceList);
+  const index = niceList.findIndex((n) => n === name);
+  console.log(index);
 
-  const { data: gift } = await axios.post(`${serverUrl}/gift`, {
-    // TODO: add request body parameters here!
-  });
+  //Creating merkle tree root hash
+  const root = merkleTree.getRoot();
+  console.log(typeof root);
 
-  console.log({ gift });
+  //Creating proof
+  const proof = merkleTree.getProof(index);
+
+  try {
+    const { data: gift } = await axios.post(`${serverUrl}/gift`, {
+      // TODO: add request body parameters here!
+      index: index,
+      proof: proof,
+      root: root,
+      name: name,
+    });
+    console.log({ gift });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-main();
+const nameArg = process.argv[2];
+main(nameArg);
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express");
+const verifyProof = require("../utils/verifyProof");
 
 const port = 1225;
 
@@ -8,20 +8,24 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT =
+  "57cb30a393e2d0570fdcffa681b42aa584f6f70e0f615b443956ae840028f408";
 
-app.post('/gift', (req, res) => {
+app.post("/gift", (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { proof, name } = req.body;
+  console.log("Request Body:", name);
+  console.log("Request Body.proof:", proof);
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
-    res.send("You got a toy robot!");
-  }
-  else {
-    res.send("You are not on the list :(");
-  }
+  // TODO: prove that a name is in the list
+  let verify = verifyProof(proof, name, MERKLE_ROOT);
+  console.log(`Esto es verify:${verify}`);
+
+  const isInTheList = verify;
+
+  isInTheList
+    ? res.send("You got a toy robot!")
+    : res.send("You are not on the list :(");
 });
 
 app.listen(port, () => {
